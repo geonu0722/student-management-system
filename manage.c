d8837ec479a55b1b6a32acaa8236295d1ccc4354
geonu0722 <88758360+geonu0722@users.noreply.github.com>
2026-08-04 23:06:47 +0900

    Update manage.c
    search update

--- manage.c
+++ manage.c
@@ -24,3 +24,20 @@ void Show_stu(STU *students, int count){
     }
     printf("-------------------------\n\n");
 }
+void Search_stu(STU *students, int count){
+    char name[30];
+    printf("what is name? : ");
+    scanf("%s", name);
+    for(int i = 0;i < count;i++){
+        if(strcmp(students[i].name, name) == 0){
+            printf("========== Student Found ==========\n\n");
+            printf("ID     :  %d\n", students[i].id);
+            printf("Name     :  %s\n", students[i].name);
+            printf("Age     :  %d\n", students[i].age);
+            printf("Score     :  %.2lf\n\n", students[i].score);
+            printf("-------------------------");
+            break;
+        }
+        if(i == count - 1) printf("no name");
+    }
+}
