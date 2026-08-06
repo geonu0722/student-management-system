986d768ada029e591ef8a55c94c29fd97f6b9ce7
geonu0722 <88758360+geonu0722@users.noreply.github.com>
2026-08-06 23:40:44 +0900

    Update main.c

--- main.c
+++ main.c
@@ -22,8 +22,10 @@ int main()
         printf("========== Student Management ==========\n");
         printf("1. Add Student\n");
         printf("2. Show Students\n");
-        printf("3. Exit\n");
-        printf("4. Search Student\n\n");
+        printf("3. Search Student\n");
+        printf("4. Update Student\n");
+        printf("5. Delete Student\n");
+        printf("6. Exit\n\n");
         printf("Choose number\n");
         scanf("%d", &num);
         switch(num){
@@ -35,9 +37,17 @@ int main()
             Show_stu(students, count);
             break;
         case 3:
-            return 0;
-        case 4:
             Search_stu(students, count);
+            break;
+        case 4:
+            Update_stu(students, count);
+            break;
+        case 5:
+            Delete_stu(students, count);
+            count--;
+            break;
+        case 6:
+            return 0;
         default:
             printf("Wrong number");
             break;
