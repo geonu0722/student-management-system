f4318fb2cda72a97ed3f539a5277aea3a73ca8c3
geonu0722 <88758360+geonu0722@users.noreply.github.com>
2026-08-06 23:40:31 +0900

    Update manage.c

--- manage.c
+++ manage.c
@@ -12,16 +12,23 @@ void Add_stu(STU *stu){
     printf("score? : ");
     scanf("%lf", &stu->score);
 }
+void Print_stu(STU *stu){
+    printf("ID     :  %d\n", stu->id);
+    printf("Name   :  %s\n", stu->name);
+    printf("Age    :  %d\n", stu->age);
+    printf("Score  :  %.2lf\n\n", stu->score);
+    printf("-------------------------");
+}
 void Show_stu(STU *students, int count){
     printf("========== Student List ==========\n\n");
     for(int i = 0;i < count;i++){
         printf("Student %d\n", i+1);
-        printf("ID     :  %d\n", students[i].id);
-        printf("Name     :  %s\n", students[i].name);
-        printf("Age     :  %d\n", students[i].age);
-        printf("Score     :  %.2lf\n\n", students[i].score);
-        printf("-------------------------");
+        Print_stu(&students[i]);
     }
+    if(count == 0){
+        printf("No student data.\n");
+        return;
+    }   
     printf("-------------------------\n\n");
 }
 void Search_stu(STU *students, int count){
@@ -31,13 +38,58 @@ void Search_stu(STU *students, int count){
     for(int i = 0;i < count;i++){
         if(strcmp(students[i].name, name) == 0){
             printf("========== Student Found ==========\n\n");
-            printf("ID     :  %d\n", students[i].id);
-            printf("Name     :  %s\n", students[i].name);
-            printf("Age     :  %d\n", students[i].age);
-            printf("Score     :  %.2lf\n\n", students[i].score);
-            printf("-------------------------");
-            break;
+            Print_stu(&students[i]);
+            return;
         }
-        if(i == count - 1) printf("no name");
     }
+    printf("no name\n");
 }
+void Update_stu(STU *students, int count){
+    int id;
+    printf("what is id? : ");
+    scanf("%d", &id);
+    for(int i = 0;i<count;i++){
+        if(id == students[i].id){
+            Print_stu(&students[i]);
+            printf("-------------------------");
+            printf("name? : ");
+            scanf("%s", students[i].name);
+            printf("age? : ");
+            scanf("%d", &students[i].age);
+            printf("score? : ");
+            scanf("%lf", &students[i].score);
+            printf("Update Complete!\n");
+            return;
+        }
+    }
+   printf("Student not found.\n");
+}
+
+void Delete_stu(STU *students, int count){
+    char name[30];
+    printf("what is name? : ");
+    scanf("%s", name);
+    for(int i = 0;i<count;i++){
+        if(strcmp(name, students[i].name) == 0){
+            for(int j = i;j<count-1;j++){
+                students[j] = students[j+1];
+            }
+            printf("Delete Complete!\n");
+            return;
+        }
+    }
+    printf("Student not found.\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
